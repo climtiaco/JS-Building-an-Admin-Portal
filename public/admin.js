aba64149e56e480f3296da71d5ba1ef483f96d95
Christian Limtiaco <climtiaco.code@gmail.com>
2023-08-14 07:58:23 -0700

    Completed Assignment

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,41 @@
+async function bookList(){
+   let response =  await fetch('http://localhost:3001/listBooks')
+   let books = await response.json();
+   const bookList = document.getElementById('bookList');
 
-// Your Code Here
+   books.forEach(book => {
+        const li = document.createElement('li');
+        li.textContent = `${book.title}`;
+        bookList.appendChild(li);
+
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.id = `quantity_${book.id}`;
+        input.name = 'quantity'
+        input.value = `${book.quantity}`;
+        bookList.appendChild(input);
+
+        const submit = document.createElement('button');
+        submit.type = 'button';
+        submit.textContent = 'Submit'
+        bookList.appendChild(submit);
+
+        submit.addEventListener('click', async () => {
+            const newQuantity = document.getElementById(`quantity_${book.id}`).value;
+
+            const updateResponse = await fetch(`http://localhost:3001/updateBook`, {
+                method: 'PATCH',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: book.id, 
+                    quantity: newQuantity })
+            });
+
+        })
+
+   })
+}
+
+bookList();
